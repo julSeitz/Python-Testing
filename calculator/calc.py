7c3a6ce93312aa978312e3d0e5f59eb329dc9340
Julian Seitz <112495164+julSeitz@users.noreply.github.com>
2025-06-10 15:31:58 +0200

    added function for subtraction and tests for it

--- calculator/calc.py
+++ calculator/calc.py
@@ -1,2 +1,5 @@
 def add(number_one, number_two):
     return number_one + number_two
+
+def sub(number_one, number_two):
+    return number_one - number_two
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,5 +1,6 @@
 import pytest
 from calculator.calc import add
+from calculator.calc import sub
 
 def test_add():
     assert add(6, 8) == 14
@@ -8,3 +9,15 @@ def test_add():
     assert add(0, 3) == 3
     assert add(6, -3) == 3
     assert add(9, 0) == 9
+
+def test_sub():
+    assert sub(6, 3) == 3
+    assert sub(5, 7) == -2
+    assert sub(-2, -5) == 3
+    assert sub(-6, -4) == -2
+    assert sub(-2, 8) == -10
+    assert sub(-4, 3) == -7
+    assert sub(4, -7) == 11
+    assert sub(8, -5) == 13
+    assert sub(1, 0) == 1
+    assert sub(0, 5) == -5
